415c2a01549912e13d3c898d7af83775a5b4acf0
Nicholas <berubenic@gmail.com>
2020-10-25 14:09:33 -0400

    board tests for grid

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,6 +6,22 @@ module TicTacToe
       it 'initializes the board with a grid' do
         expect { Board.new(grid: 'grid') }.to_not raise_error
       end
+      it 'sets the grid with three rows by default' do
+        board = Board.new
+        expect(board.grid.size).to eq(3)
+      end
+      it 'creates three things in each row by default' do
+        board = Board.new
+        board.grid.each do |row|
+          expect(row.size).to eq(3)
+        end
+      end
+    end
+    context '#grid' do
+      it 'returns the grid' do
+        board = Board.new(grid: 'blah')
+        expect(board.grid).to eq 'blah'
+      end
     end
   end
 end
