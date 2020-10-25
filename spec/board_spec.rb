1a9ee8403b5f4837ec681a197d7e12d97c292cf0
Nicholas <berubenic@gmail.com>
2020-10-25 14:13:41 -0400

    get cell method for board and test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,10 +6,12 @@ module TicTacToe
       it 'initializes the board with a grid' do
         expect { Board.new(grid: 'grid') }.to_not raise_error
       end
+
       it 'sets the grid with three rows by default' do
         board = Board.new
         expect(board.grid.size).to eq(3)
       end
+
       it 'creates three things in each row by default' do
         board = Board.new
         board.grid.each do |row|
@@ -17,10 +19,19 @@ module TicTacToe
         end
       end
     end
+
     context '#grid' do
       it 'returns the grid' do
         board = Board.new(grid: 'blah')
         expect(board.grid).to eq 'blah'
+      end
+    end
+
+    context '#get_cell' do
+      it 'return the cell based on the (x, y) coordinate' do
+        grid = [['', '', ''], ['', '', 'something'], ['', '', '']]
+        board = Board.new(grid: grid)
+        expect(board.get_cell(2, 1)).to eq 'something'
       end
     end
   end
