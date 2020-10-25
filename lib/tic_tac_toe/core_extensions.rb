1e460b9e14f0919fc94001a484c2e25f9a12c6bd
Nicholas <berubenic@gmail.com>
2020-10-25 17:58:10 -0400

    method all_same? for Array and tests

--- lib/tic_tac_toe/core_extensions.rb
+++ lib/tic_tac_toe/core_extensions.rb
@@ -2,4 +2,8 @@ class Array
   def all_empty?
     all? { |element| element.to_s.empty? }
   end
+
+  def all_same?
+    all? { |element| element == self[0] }
+  end
 end
--- spec/core_extensions_spec.rb
+++ spec/core_extensions_spec.rb
@@ -14,4 +14,18 @@ describe Array do
       expect([].all_empty?).to be true
     end
   end
+
+  context '#all_same?' do
+    it 'returns true if all elements of the Array are the same' do
+      expect(%w[A A A].all_same?).to be true
+    end
+
+    it 'returns false if some of the Array elements are not the same' do
+      expect(['', 1, '', Object.new, :a].all_same?).to be false
+    end
+
+    it 'returns true for an empty Array' do
+      expect([].all_same?).to be true
+    end
+  end
 end
