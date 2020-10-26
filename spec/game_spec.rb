aa58b2de6a862ba8b95f5508102fa6cb633d36f1
Nicholas <berubenic@gmail.com>
2020-10-26 18:26:43 -0400

    init Game class and test

---
+++ spec/game_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+module TicTacToe
+  describe Game do
+    let(:bob) { Player.new({ color: 'X', name: 'bob' }) }
+    let(:frank) { Player.new({ color: 'O', name: 'frank' }) }
+    let(:game) { Game.new([bob, frank]) }
+
+    context 'initialize' do
+      it 'randomly selects a current_player' do
+        allow_any_instance_of(Array).to receive(:shuffle) { [frank, bob] }
+        game = Game.new([bob, frank])
+        expect(game.current_player).to eq frank
+      end
+
+      it 'randomly selects an other player' do
+        allow_any_instance_of(Array).to receive(:shuffle) { [frank, bob] }
+        game = Game.new([bob, frank])
+        expect(game.other_player).to eq bob
+      end
+    end
+  end
+end
